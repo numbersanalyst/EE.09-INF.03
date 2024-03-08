4367525cf26755e2571ed61224ce8241a8541243
Numbers analyst <124043202+numbersanalyst@users.noreply.github.com>
2024-03-08 12:26:05 +0100

    Add a part of solution for new exam

---
+++ EE.09-05-22.01-SG/uzytkownicy.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal społecznościowy</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+
+<body>
+    <header>
+        <section>
+            <h2>Nasze osiedle</h2>
+        </section>
+        <section>
+            <!-- efekt dzialania skryptu 1 -->
+        </section>
+    </header>
+
+    <main>
+        <section>
+            <h3>Logowanie</h3>
+            <form action="uzytkownicy.php" method="post">
+                <p>login</p>
+                <input type="text" name="login" id="login">
+                <p>hasło</p>
+                <input type="password" name="password" id="password">
+            </form>
+        </section>
+        <section>
+            <h3>Wizytówka</h3>
+            <section>
+                <!-- efekt dzialania skryptu 2 -->
+            </section>
+        </section>
+    </main>
+
+    <footer>
+        <p>Stronę wykonał: 000000000</p>
+    </footer>
+</body>
+
+</html>
