f54799669c94ca65b5278fe5afe035dcef7d1310
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2024-05-27 12:48:26 +0200

    Add soultion to EE.09-06-22.01-SG exam

---
+++ EE.09-06-22.01-SG/main.js
@@ -0,0 +1,28 @@
+const prawy = document.getElementById('prawy')
+const obraz = document.getElementById('obraz')
+const lista = document.getElementById('lista')
+
+const kolorBg = (kolor) => {
+    prawy.style.backgroundColor = kolor
+}
+
+const kolorTekstu = (kolor) => {
+    prawy.style.color = kolor.value
+    console.log(kolor)
+}
+
+const zmienObramowanie = (zaznaczony) => {
+    if (zaznaczony) {
+        obraz.style.border = null
+    } else {
+        obraz.style.border = "none"
+    }
+}
+
+const rozmiarTekstu = (rozmiar) => {
+    prawy.style.fontSize = rozmiar
+} 
+
+const stylListy = (styl) => {
+    lista.style.listStyleType = styl
+}
