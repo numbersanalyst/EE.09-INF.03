0f5e5b77784321d5af005b12993ce2d697e25e75
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2024-05-28 11:26:19 +0200

    Add solution to HTML part for EE.09-05-21.01-SG exam

---
+++ EE.09-05-21.01-SG/lotnisko.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Port Lotniczy</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+    <header>
+        <div class="h-1">
+            <img src="zad5.png" alt="logo lotnisko">
+        </div>
+        <div class="h-2">
+            <h1>Przyloty</h1>
+        </div>
+        <div class="h-3">
+            <h3>przydatne linki</h3>
+            <a href="kwerendy.txt" target="_blank">Pobierz...</a>
+        </div>
+    </header>
+    
+    <main>
+        <table>
+            <tr>
+                <th>czas</th>
+                <th>kierunek</th>
+                <th>numer rejsu</th>
+                <th>status</th>
+            </tr>
+            <!-- skrypt 1 -->
+        </table>
+    </main>
+
+    <footer>
+        <div class="f-1">
+            <!-- skrypt 2 -->
+        </div>
+        <div class="f-2">Autor: 000000000000</div>
+    </footer>
+</body>
+</html>
